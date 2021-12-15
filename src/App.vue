d76bd15003f11706bd57dd2148890c16dd0d8e4f
garretthensley <garrettmichaelhensley@gmail.com>
2021-12-15 11:40:49 -0500

    first commit

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <the-header></the-header>
+  <button @click="addThing">add a thing</button>
+  <router-view></router-view>
+</template>
+
+<script>
+import TheHeader from './components/nav/TheHeader.vue';
+
+export default {
+  components: {
+    TheHeader,
+  },
+
+  methods: {
+    addThing() {
+      this.$store.commit('addProductToCart', this.products[0]);
+    },
+  },
+};
+</script>
+
+<style>
+* {
+  box-sizing: border-box;
+}
+
+html {
+  font-family: sans-serif;
+}
+
+body {
+  margin: 0;
+}
+</style>
